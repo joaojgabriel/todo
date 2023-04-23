531814ff4c415469b06c5c83203537671b073b10
Joao <jonhlhn.surf@gmail.com>
2023-04-23 14:28:48 -0300

    Add task object to inbox onclick of +

---
+++ src/Logic.js
@@ -0,0 +1,3 @@
+export function createTaskObject(task) {
+  return { task, dueDate: null, completed: false };
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './style.css'
+import "./style.css";
+import * as lg from "./Logic";
+
+const inbox = [];
+const addTaskButton = document.querySelector("button#add-task");
+const newTaskInput = document.querySelector("input#new-task");
+
+// When user clicks + Add Task
+addTaskButton.addEventListener("click", () => {
+  // Create a task object and add it to inbox list
+  const newTask = lg.createTaskObject(newTaskInput.value);
+  if (!newTask) return;
+  inbox.push(newTask);
+});
