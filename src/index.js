046c65aaa637caa944d7cbcf2cbad75ad488bc84
Joao <jonhlhn.surf@gmail.com>
2023-04-26 10:24:09 -0300

    Index tasks to modify them individually

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import * as lg from "./Logic";
 const Context = (() => {
   let current = "default";
   const map = { default: [] };
+  let count = 0;
   return {
     getCurrent() {
       return current;
@@ -11,14 +12,17 @@ const Context = (() => {
     change(context) {
       current = context;
     },
-    addTask(taskObject) {
-      map[current].push(taskObject);
+    indexTask(taskObject) {
+      count += 1;
+      const indexedTask = { ...taskObject, index: count };
+      return indexedTask;
     },
-    modifyTask({ name, context }, changeObject) {
-      // This has an inherent bug: tasks with the same
-      // name and context are all modified
+    addTask(indexedTask) {
+      map[current].push(indexedTask);
+    },
+    modifyTask({ context, index }, changeObject) {
       map[context].forEach((task) => {
-        if (task.name === name) {
+        if (task.index === index) {
           [...Object.entries(changeObject)].forEach(([key, value]) => {
             // eslint-disable-next-line no-param-reassign
             task[key] = value;
@@ -44,18 +48,22 @@ plusButton.addEventListener("click", () => {
   const dueDate = dueDateInput.value;
   const context = Context.getCurrent();
 
-  const newTaskObject = { name, dueDate, context, completed: false };
-  Context.addTask(newTaskObject);
-  const newTaskElement = lg.createTaskElement(newTaskObject);
+  const indexedTask = Context.indexTask({
+    name,
+    dueDate,
+    context,
+    completed: false,
+  });
+  Context.addTask(indexedTask);
+  const newTaskElement = lg.createTaskElement(indexedTask);
 
   const newTaskCheckbox = newTaskElement.querySelector(`#${name}-checkbox`);
   // const newTaskEditButton = newTaskElement.querySelector('.edit')
   // const newTask
   newTaskCheckbox.addEventListener("change", () => {
     newTaskElement.classList.toggle("completed");
-    Context.modifyTask(newTaskObject, { completed: newTaskCheckbox.checked });
+    Context.modifyTask(indexedTask, { completed: newTaskCheckbox.checked });
   });
-
 
   taskList.append(newTaskElement);
 });
