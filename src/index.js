14059ea69f67e1bfbe342f704bd10c73a268f108
Joao <jonhlhn.surf@gmail.com>
2023-04-26 00:32:41 -0300

    Create new task when pressing +

--- src/index.js
+++ src/index.js
@@ -3,19 +3,34 @@ import * as lg from "./Logic";
 
 const Context = (() => {
   let current = "default";
-  const list = [{ default: [] }];
+  const map = { default: [] };
   return {
-    current() {
+    getCurrent() {
       return current;
     },
     change(context) {
       current = context;
     },
+    addTask(taskObject) {
+      map[current].push(taskObject);
+    },
+    modifyTask({ name, context }, changeObject) {
+      // This has an inherent bug: tasks with the same
+      // name and context are all modified
+      map[context].forEach((task) => {
+        if (task.name === name) {
+          [...Object.entries(changeObject)].forEach(([key, value]) => {
+            // eslint-disable-next-line no-param-reassign
+            task[key] = value;
+          });
+        }
+      });
+    },
   };
 })();
 
 const newTaskInput = document.querySelector("input#new-task");
-const dueDate = document.querySelector("input#due-date");
+const dueDateInput = document.querySelector("input#due-date");
 const plusButton = document.querySelector("button#add-task");
 const taskList = document.querySelector("ul#task-list");
 
@@ -23,8 +38,25 @@ const nav = document.querySelector("nav");
 const inboxButton = document.querySelector("button#inbox");
 const addProjectButton = document.querySelector("button#add-project");
 
-plusButton.addEventListener("click", () => {});
+plusButton.addEventListener("click", () => {
+  const name = newTaskInput.value || null;
+  if (!name) return;
+  const dueDate = dueDateInput.value;
+  const context = Context.getCurrent();
 
-inboxButton.addEventListener("click", () => {})
+  const newTaskObject = { name, dueDate, context, completed: false };
+  Context.addTask(newTaskObject);
+  const newTaskElement = lg.createTaskElement(newTaskObject);
+
+  const newTaskCheckbox = newTaskElement.querySelector(`#${name}-checkbox`);
+  newTaskCheckbox.addEventListener("change", () => {
+    newTaskElement.classList.toggle("completed");
+    Context.modifyTask(newTaskObject, { completed: newTaskCheckbox.checked });
+  });
+
+  taskList.append(newTaskElement);
+});
+
+inboxButton.addEventListener("click", () => {});
 
 addProjectButton.addEventListener("click", () => {});
